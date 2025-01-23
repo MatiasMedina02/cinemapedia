6d14906311752f319ac40294d766a507893da5d8
Matias Medina <matias.workms@gmail.com>
2025-01-23 11:11:22 -0300

    Variables de entorno - Config

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,7 +10,9 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Home Screen"),
       ),
-      body: Placeholder(),
+      body: Center(
+        child: Text(Enviroment.movieDbKey),
+      ),
     );
   }
 }
